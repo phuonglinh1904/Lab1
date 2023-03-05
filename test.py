cb72eba308bb31939f8122966392f5ede285ef89
phuonglinh1904 <97249601+phuonglinh1904@users.noreply.github.com>
2023-03-05 11:38:40 +0300

    fix

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import func1 as func
-import gradient_descent as const_lr
+from gradient_descent import grad_desc
 
 def plot_function(function):
     X, Y = np.meshgrid(np.linspace(-3.0, 3.0, 100),
@@ -24,12 +24,12 @@ X, Y = np.meshgrid(x, y)
 F = func.f(X, Y)
 x_0, y_0 = 1, 1
 epsilon = 0.0001
-points = const_lr.grad_desc(x_0=x_0, y_0=y_0, learning_rate=0.01, epsilon=epsilon)
-#ax = plt.subplots()[1]
+points = grad_desc(x_0=x_0, y_0=y_0, learning_rate=0.01, epsilon=epsilon)
+ax = plt.subplots()[1]
 horizontal_axis = np.array(points[0:len(points):2])
 vertical_axis = np.array(points[1:len(points):2])
-plt.scatter(horizontal_axis, vertical_axis, c ="blue")
-#ax.plot(x,y)
-#ax.contour(X, Y, F, 40)
+plt.scatter(horizontal_axis, vertical_axis, marker='*', color='red')
+ax.plot(x,y)
+ax.contour(X, Y, F, 50)
 plt.show()
 
--- test_one_dimensional_search_methods.py
+++ test_one_dimensional_search_methods.py
@@ -4,48 +4,44 @@ from fibonacci import fibonacci_search
 from golden_ratio import golden_ratio_search
 import matplotlib.pyplot as plt
 import pandas as pd
-def test_function_1(x):
+def function_1(x):
     return np.sin(x) - x**3 - 1
 
 
-def test_function_2(x):
+def function_2(x):
     return np.log2(x) * np.cos(x) + 4
 
 
-def test_function_3(x):
+def function_3(x):
     return np.sin(x - 1)
 
-precisions = np.linspace(0.00001, 0.1, 50)
 
+borders = [[-1000, 1000], [-10, 100000], [-1000, 1]]
+precisions = np.linspace(0.00001, 0.1, 50)
 
 def plot_figure(method, color, n_iter=0, label="", func=None):
     if func is None:
-        func = test_function_2
+        func = function_1
 
     iterations = np.array([])
     for precision in precisions:
         if n_iter == 0:
-            func_res = np.array(method(func, -1, 5, precision)[2])
+            func_res = np.array(method(func, 1, 10, precision)[2])
         else:
-            func_res = np.array(method(func, -1, 5, precision, n_iter)[2])
+            func_res = np.array(method(func, 1, 10, precision, n_iter)[2])
         iterations = np.append(iterations, func_res.shape[0])
-
+    plt.xlabel("precision")
+    plt.ylabel("iteration")
     plt.plot(precisions, iterations, color=color, label=label)
-plt.xlabel("precision")
-plt.ylabel("iteration")
-#plot_figure(dichotomy_method, "blue", label='dichotomy', func=test_function_2)
-#plot_figure(fibonacci_search, "blue",n_iter=40, label='fibonaci', func=test_function_2)
-#plot_figure(golden_ratio_search, "blue", label='golden', func=test_function_2)
-plt.legend()
-plt.show()
-
-def plot_graph(method,func=None) :
-    x_min, y_min, iter_data = dichotomy_method(test_function_1,-1,5,0.01)
-    x = np.arange(1,7)
-    y = test_function_1(x)
-    plt.plot(x,y,'b-', x_min, y_min,'go',iter_data[0:len(iter_data):2], iter_data[:len(iter_data):2],'r^')
-    plt.xlabel("X label")
-    plt.ylabel("Y label")
+    plt.legend()
     plt.show()
+#plot_figure(dichotomy_method, "blue", label='dichotomy', func=function_2)
+#plot_figure(fibonacci_search, "blue",n_iter=40, label='fibonaci', func=function_2)
+#plot_figure(golden_ratio_search, "blue", label='golden', func=function_2)
 
-plot_graph(dichotomy_method,test_function_1)
+
+
+
+
+
+
